ab3c8f8515f9e30575c6b73536e073a85822d2b6
sara87086 <sarakhan87086@gmail.com>
2026-02-10 22:08:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Umair Khan',
+    author_email='umairkhan@example.com',
+    packages=find_packages(), 
+    install_requires=[]    
+)
